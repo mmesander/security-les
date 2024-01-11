f3282bfe0209fab9db81c49f993f5723e986f19a
mmesander <mark_mesander@hotmail.com>
2024-01-11 14:41:53 +0100

    clean project - add lombok

--- src/main/java/nl/novi/les16jwt/model/Role.java
+++ src/main/java/nl/novi/les16jwt/model/Role.java
@@ -4,23 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "roles")
 public class Role {
+    // Variables
     @Id
     private String rolename;
 
+    // Relations
     @ManyToMany(mappedBy = "roles")
     private Set<User> users;
-
-    public String getRolename() {
-        return rolename;
-    }
-
-    public void setRolename(String rolename) {
-        this.rolename = rolename;
-    }
 }
